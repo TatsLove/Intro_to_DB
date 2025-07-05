fbe45bbda8cdb5747aa440cd05522cd23224ba33
Tatenda Loverage Tirivavi <tloverage@gmail.com>
2025-07-05 12:14:51 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,21 @@
-USE alx_book_store;
+SHOW FULL COLUMNS FROM alx_book_store.books;
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Books'
-  AND TABLE_SCHEMA = 'alx_book_store';
+
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
+
+
+With SQL keywords in uppercase:
+
+
+SELECT 
+becomes SELECT stays the same 
+    COLUMN_NAME, 
+    COLUMN_TYPE 
+FROM becomes FROM is a keyword and will be uppercased to FROM
